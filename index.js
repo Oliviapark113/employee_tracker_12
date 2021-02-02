1082b194bf4e44e9a73754d411c68788c1c82974
Olivia Park <oliviaypark113@gmail.com>
2021-02-01 19:47:11 -0500

    set up intro fuction with inquirer

--- index.js
+++ index.js
@@ -13,5 +13,53 @@ const connection = mysql.createConnection({
 connection.connect(err=>{
     if(err) throw err
     console.log(`connected as id${connection.threadId}`)
+    intro();
 
 })
+
+const intro = () =>{
+
+   inquirer.prompt([{
+      name: "choice",
+      type: "list",
+      message: "What would you like to do?",
+      choices :["View All Employees By Department",
+                "View All Employees By Manager",
+                "Add Employee",
+                "Remove Employee",
+                "Update Employee Role",
+                "Update Employee Manager",
+                "View All Roles"
+                ]
+
+        }]).then(answer =>{
+            if(answer.choice === "View All Employees By Department"){
+                readAllEmployeeByDept();
+            }
+            else if (answer.choice === "View All Employees By Manager" ){
+                readAllManager();
+            }
+
+            else if (answer.choice === "Add Employee" ){
+                addEmployee();
+            }
+
+            else if (answer.choice === "Remove Employee" ){
+                removeEmployee();
+            }
+
+            else if (answer.choice === "Update Employee Role" ){
+                updateEmployeeRole();
+            }
+
+            else if (answer.choice === "Update Employee Manager" ){
+                updateEmployeeManager();
+            }
+
+            else if (answer.choice === "View All Roles" ){
+                updateEmployeeManager();
+            }
+        })
+
+
+}
